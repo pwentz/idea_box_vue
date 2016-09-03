084bab31549fca461780d71c83549f06024c7eb0
pwentz <patrick.w.wentz@gmail.com>
2016-09-03 15:25:32 -0600

    ideas can be created and immediately rendered

--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -2,6 +2,17 @@ class Api::V1::IdeasController < ApplicationController
   respond_to :json
 
   def index
-    respond_with Idea.all
+    respond_with Idea.all.reverse
+  end
+
+  def create
+    idea = Idea.create(idea_params)
+    respond_with :api, :v1, idea
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   #
   get '/ideas', to: 'ideas#index', as: 'ideas'
 
-  namespace 'api' do
-    namespace 'v1' do
-      resources :ideas, only: [:index]
+  namespace :api do
+    namespace :v1 do
+      resources :ideas
     end
   end
 end
